ddc145ec601b2d996e803542509e675632b617e1
anandvipul <anandvipul01@gmail.com>
2024-05-09 09:00:33 +0530

    Form Validation

--- index.php
+++ index.php
@@ -8,9 +8,42 @@
 <body>
 <?php 
 
-echo "<h1> Forms <h1>";
+echo "<h1> Forms </h1>";
 
 ?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    Name: <input type="text" name="name"> <br>
+    E-mail: <input type="text" name="email"><br>
+    Website: <input type="text" name="website"><br>
+    Comment: <textarea type="comment" name="comment" rows="5" cols="40"></textarea><br>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+<br>
+    <input type="submit" >
+</form>
+
+<?php 
+
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $website = test_input($_POST["website"]);
+    $comment = test_input($_POST["comment"]);
+    $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+?>
+
 </body>
 </html>
 
