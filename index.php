7af0c23eced8503b188389ae799749bfbfdc3cb8
anandvipul <anandvipul01@gmail.com>
2024-05-09 10:28:45 +0530

    form required modify

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php 
@@ -11,29 +12,19 @@
 echo "<h1> Forms </h1>";
 
 ?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    Name: <input type="text" name="name"> <br>
-    E-mail: <input type="text" name="email"><br>
-    Website: <input type="text" name="website"><br>
-    Comment: <textarea type="comment" name="comment" rows="5" cols="40"></textarea><br>
-    Gender:
-    <input type="radio" name="gender" value="female">Female
-    <input type="radio" name="gender" value="male">Male
-    <input type="radio" name="gender" value="other">Other
-<br>
-    <input type="submit" >
-</form>
 
 <?php 
 
 $name = $email = $gender = $comment = $website = "";
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $website = test_input($_POST["website"]);
-    $comment = test_input($_POST["comment"]);
-    $gender = test_input($_POST["gender"]);
+
+    empty($_POST["name"]) ? $nameErr = "name is required" : $name = test_input($_POST["name"]);
+    empty($_POST['email']) ? $emailErr = "Email is Required" : $email = test_input($_POST["email"]);
+    empty($_POST["gender"]) ? $genderErr = "Gender is required" : $gender = test_input(($_POST["gender"]));
+    empty($_POST["website"]) ? $website = "" : $website = test_input($_POST["website"]);
+    empty($_POST["comment"]) ? $comment = "" : $comment = test_input($_POST["comment"]);
 }
 
 function test_input($data) {
@@ -43,6 +34,23 @@ $data = htmlspecialchars($data);
 return $data;
 }
 ?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<p class="error">* required field</p>
+    Name: <input type="text" name="name"><span class="error"><?php echo $nameErr ?> </span><br>
+    E-mail: <input type="text" name="email"> <span class="error"> <?php echo $emailErr ?></span><br>
+    Website: <input type="text" name="website"><span class="error"><?php echo $websiteErr ?></span><br>
+    Comment: <textarea type="comment" name="comment" rows="5" cols="40"></textarea><br>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+    <span class="error"><?php echo $genderErr ?></span>
+<br>
+    <input type="submit" >
+</form>
+
+
 
 </body>
 </html>
