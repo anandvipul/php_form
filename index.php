5cb74d8e5131dfd633cc041eb61adb7999ac5557
anandvipul <anandvipul01@gmail.com>
2024-05-09 12:05:06 +0530

    form email url name validation and variable retention added

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo "<h1> Forms </h1>";
 
 $name = $email = $gender = $comment = $website = "";
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
-
+echo $name;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // empty($_POST["name"]) ? $nameErr = "name is required" : $name = test_input($_POST["name"]);
@@ -59,20 +59,41 @@ return $data;
 }
 ?>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-<p class="error">* required field</p>
-    Name: <input type="text" name="name"><span class="error"><?php echo $nameErr ?> </span><br>
-    E-mail: <input type="text" name="email"> <span class="error"> <?php echo $emailErr ?></span><br>
-    Website: <input type="text" name="website"><span class="error"><?php echo $websiteErr ?></span><br>
-    Comment: <textarea type="comment" name="comment" rows="5" cols="40"></textarea><br>
-    Gender:
-    <input type="radio" name="gender" value="female">Female
-    <input type="radio" name="gender" value="male">Male
-    <input type="radio" name="gender" value="other">Other
-    <span class="error"><?php echo $genderErr ?></span>
-<br>
-    <input type="submit" >
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
 </form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
 
 
